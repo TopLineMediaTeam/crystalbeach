878738752e0cdcd2d80bc1eeadd613c2feee7d7c
Russ <Russ@CodeJunky>
2016-02-06 10:00:46 -0500

    saving to push to Git

---
+++ wp-content/themes/crystalbeach.com/functions.php
@@ -0,0 +1,28 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
+
+?>
+<?php
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Add sidebar',
+		'id'            => 'add_left',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
+?>
+
